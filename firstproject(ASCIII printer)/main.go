39b49ff7ac04d7a8fb92c2c4bd670afacf6160b7
den990 <200321ds>
2024-01-24 16:24:59 +0300

    Add first programm

--- firstproject(ASCIII printer)/main.go
+++ firstproject(ASCIII printer)/main.go
@@ -11,13 +11,9 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	// Получаем случайное слово для отображения в ASCII-арт
 	word := getRandomWord()
-
-	// Создаем объект Figlet
 	asciiRender := figlet4go.NewAsciiRender()
 
-	// Настройки для ASCII-арта
 	options := figlet4go.NewRenderOptions()
 	options.FontColor = []figlet4go.Color{
 		figlet4go.ColorRed,
@@ -34,17 +30,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Выводим ASCII-арт
 	fmt.Println(renderStr)
 }
 
 func getRandomWord() string {
-	// Пример списка слов для ASCII-арта
 	words := []string{"Gopher", "Hello", "Go", "Art", "ASCII", "Dangerousis"}
 
-	// Генерируем случайный индекс из списка слов
 	randomIndex := rand.Intn(len(words))
 
-	// Возвращаем случайное слово
 	return words[randomIndex]
 }
